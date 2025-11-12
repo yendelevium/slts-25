1e5d21c7f0d955be9d5e7381a80db47823122176
yendelevium <yashbardia27@gmail.com>
2025-11-12 19:32:42 +0530

    feat: finish components for StudentInfo section

--- src/components/form/StudentInfo.tsx
+++ src/components/form/StudentInfo.tsx
@@ -1,6 +1,5 @@
 
 import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
 import {
   Field,
   FieldDescription,
@@ -23,11 +22,22 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default function StudentInfo() {
     // TODO: Replace these states with the TanStack store later when I actually write the store lol
     // The states for which group the student is in, and whether he has passed the group 2 exam or not (only if grp 3 is selected)
     const [activeGroup, setActiveGroup] = useState<string | null>(null)
+    const [activeGender, setActiveGender] = useState<string | null>(null)
     const [passed, setPassed] = useState<string | null>(null)
 
     // DOB state
@@ -62,13 +72,78 @@ export default function StudentInfo() {
         )
     });
 
+    const genderElementsJSX = ["Male", "Female", "Other"].map(gender => {
+        return(
+            <Button
+            key={gender}
+            onClick={()=>{
+                // TODO: Update store to set the chosen gender...
+                setActiveGender(gender)
+            }}
+            variant="outline"
+            // size="lg"
+            className={
+                `flex items-center gap-2 cursor-pointer font-normal 
+                ${activeGender === gender ? "!bg-indigo-500 !text-white !border-indigo-500" : ""}`
+            }
+            >
+            {gender}
+            </Button>
+        )
+    });
+
+    const districts = [
+      "Coimbatore",
+      "Dharmapuri / Krishnagiri",
+      "Dindigul",
+      "Erode",
+      "Kanchipuram North",
+      "Kanchipuram South",
+      "Kanyakumari",
+      "Karur",
+      "Madurai",
+      "Namakkal",
+      "Nilgiris",
+      "Salem",
+      "Sivaganga&Ramnad",
+      "Thanjavur",
+      "Theni",
+      "Trichy",
+      "Tirunelveli",
+      "Tirupur",
+      "Tiruvannamalai",
+      "Tuticorin",
+      "Vellore",
+      "Villupuram",
+      "Virudhunagar",
+      "Chennai East Coast",
+      "Chennai North",
+      "Chennai North West",
+      "Chennai South",
+      "Chennai South East",
+      "Chennai West",
+      "Cuddalore",
+      "Nagapattinam",
+      "Puducherry",
+      "Tiruvallur East",
+      "Tiruvallur West",
+      "Mayiladuthurai",
+    ];
+
+    const districtElementsJSX = districts.map((district) => (
+    <SelectItem key={district} value={district}>
+        {district}
+    </SelectItem>
+    ));
+
+
     return (
         <div className="mb-5 bg-white rounded-lg shadow-sm p-6">
             <FieldGroup>
             <FieldSet>
                 <FieldLegend>Student Information</FieldLegend>
                 <FieldDescription>
-                Please enter the CORRECT information about the participation StudentInfo
+                Please enter the CORRECT information about the participation
                 </FieldDescription>
 
                 {/* Group selection field */}
@@ -142,27 +217,94 @@ export default function StudentInfo() {
                         </PopoverContent>
                     </Popover>
                 </Field>
-                </FieldGroup>
-            </FieldSet>
 
-            <FieldSet>
-                <FieldLegend>Billing Address</FieldLegend>
-                <FieldDescription>
-                The billing address associated with your payment method
-                </FieldDescription>
-                <FieldGroup>
-                <Field orientation="horizontal">
-                    <Checkbox
-                    id="checkout-7j9-same-as-shipping-wgm"
-                    defaultChecked
-                    />
-                    <FieldLabel
-                    htmlFor="checkout-7j9-same-as-shipping-wgm"
-                    className="font-normal"
-                    >
-                    Same as shipping address
+
+                {/* TODO: DO THE STATE SETTING FOR ALL OF THESE GUYS... */}
+                <Field>
+                    <FieldLabel htmlFor="name">
+                        Name *
                     </FieldLabel>
+                    <Input type="text" placeholder="Yash" id="name"/>
                 </Field>
+                
+                <Field>
+                    <FieldLabel htmlFor="gender">
+                        Gender *
+                    </FieldLabel>
+                        <div className="flex flex-wrap gap-3">
+                            {genderElementsJSX}
+                        </div>
+                </Field>
+
+                <Field>
+                    <FieldLabel htmlFor="district">
+                        District *
+                    </FieldLabel>
+                    <Select>
+                        <SelectTrigger className="font-normal">
+                            <SelectValue placeholder="Select your district" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectLabel>Districts</SelectLabel>
+                                {districtElementsJSX}
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                </Field>
+
+                <Field>
+                    <FieldLabel htmlFor="samithi">
+                        Samithi *
+                    </FieldLabel>
+                    <Input type="text" placeholder="idk" id="samithi"/>
+                </Field>
+                
+
+                {/* Couldn't find a year only dropdown in SHADCN, might just make it a select button later? */}
+                <Field>
+                    <FieldLabel htmlFor="year-bv">
+                        Student's Year of Joining Balvikas*
+                    </FieldLabel>
+                    <Input type="number" placeholder="2019" id="year-bv" min={2000} max={2025}/>
+                </Field>
+
+                {/* Change this to be customized for balvikas date, not the same as DOB */}
+                <Field>
+                    <FieldLabel htmlFor="date-bv">
+                        Date of Joining Balvikas (Optional)
+                    </FieldLabel>
+                    <Popover>
+                        <PopoverTrigger asChild>
+                        <Button
+                            variant="outline"
+                            id="date"
+                            className="w-[var(--radix-popover-trigger-width)] justify-between font-normal"
+                        >
+                            {date ? date.toLocaleDateString() : "Select date"}
+                            <ChevronDownIcon />
+                        </Button>
+                        </PopoverTrigger>
+                        
+                        {/* TODO: Make the pop-over calendart the same length as the button and center it */}
+                        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+                            <Calendar
+                                mode="single"
+                                selected={date}
+                                captionLayout="dropdown"
+                            />
+                        </PopoverContent>
+                    </Popover>
+                </Field>
+
+                <Field>
+                    <FieldLabel htmlFor="allergy">
+                        Food Allergies (Optional)
+                    </FieldLabel>
+                    <Input type="text" placeholder="peanuts, gluten" id="allergy"/>
+                </Field>
+
+
                 </FieldGroup>
             </FieldSet>
             </FieldGroup>
