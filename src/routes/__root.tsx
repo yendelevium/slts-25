251cb14a0d90bf4e218e4f6929f04f17b9dcb1ca
yendelevium <yashbardia27@gmail.com>
2025-11-14 20:16:10 +0530

    chore: remove tanstack dev-tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,4 @@
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanStackDevtools } from "@tanstack/react-devtools";
-
-import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
-
 import type { QueryClient } from "@tanstack/react-query";
 
 interface MyRouterContext {
@@ -14,18 +9,6 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <Outlet />
-      <TanStackDevtools
-        config={{
-          position: "bottom-right",
-        }}
-        plugins={[
-          {
-            name: "Tanstack Router",
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          TanStackQueryDevtools,
-        ]}
-      />
     </>
   ),
 });
